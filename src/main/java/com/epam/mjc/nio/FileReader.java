caadbb0a6b65ec64475848e528c65a55a69e3b94
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 02:40:47 +0300

    Update FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,12 +1,6 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class FileReader {
 
